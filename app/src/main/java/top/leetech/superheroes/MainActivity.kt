83249648243b05e2f5593781d24a6cba6a8c4b7a
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2025-08-14 02:55:57 +0300

    add heroes, topappbar and statusbarpadding

--- app/src/main/java/top/leetech/superheroes/MainActivity.kt
+++ app/src/main/java/top/leetech/superheroes/MainActivity.kt
@@ -6,14 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import top.leetech.superheroes.data.HeroesList
@@ -25,7 +31,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperheroesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            modifier = Modifier
+                                .statusBarsPadding()
+                                .padding(vertical = 12.dp)
+                        )
+                    }) { innerPadding ->
                     SuperheroesApp(
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -40,7 +55,7 @@ class MainActivity : ComponentActivity() {
 fun SuperheroesApp(modifier: Modifier = Modifier) {
     LazyColumn(
         modifier = modifier,
-        contentPadding = PaddingValues(16.dp),
+        contentPadding = PaddingValues(start = 16.dp, end = 16.dp, bottom = 8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(HeroesList.heroes) {
@@ -49,10 +64,34 @@ fun SuperheroesApp(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true, apiLevel = 35)
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.displayLarge,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true, apiLevel = 35, showSystemUi = true)
 @Composable
 fun HeroAppPreview() {
-    SuperheroesTheme {
-        SuperheroesApp()
+    SuperheroesTheme(darkTheme = true) {
+        Scaffold(
+            modifier = Modifier
+                .fillMaxSize(),
+            topBar = {
+                TopAppBar(
+                    modifier = Modifier
+                        .statusBarsPadding()
+                        .padding(vertical = 12.dp)
+                )
+            }) { innerPadding ->
+            SuperheroesApp(
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 }
--- app/src/main/java/top/leetech/superheroes/data/HeroesList.kt
+++ app/src/main/java/top/leetech/superheroes/data/HeroesList.kt
@@ -33,6 +33,56 @@ object HeroesList {
             name = R.string.hero6,
             about = R.string.description6,
             image = R.drawable.android_superhero6
+        ),
+        Hero(
+            name = R.string.hero7,
+            about = R.string.description7,
+            image = R.drawable.android_superhero1
+        ),
+        Hero(
+            name = R.string.hero8,
+            about = R.string.description8,
+            image = R.drawable.android_superhero2
+        ),
+        Hero(
+            name = R.string.hero9,
+            about = R.string.description9,
+            image = R.drawable.android_superhero6
+        ),
+        Hero(
+            name = R.string.hero10,
+            about = R.string.description10,
+            image = R.drawable.android_superhero5
+        ),
+        Hero(
+            name = R.string.hero11,
+            about = R.string.description11,
+            image = R.drawable.android_superhero6
+        ),
+        Hero(
+            name = R.string.hero12,
+            about = R.string.description12,
+            image = R.drawable.android_superhero5
+        ),
+        Hero(
+            name = R.string.hero13,
+            about = R.string.description13,
+            image = R.drawable.android_superhero1
+        ),
+        Hero(
+            name = R.string.hero14,
+            about = R.string.description14,
+            image = R.drawable.android_superhero5
+        ),
+        Hero(
+            name = R.string.hero15,
+            about = R.string.description15,
+            image = R.drawable.android_superhero2
+        ),
+        Hero(
+            name = R.string.hero16,
+            about = R.string.description16,
+            image = R.drawable.android_superhero3
         )
     )
 }
